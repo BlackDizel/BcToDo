b038fb5e8a83dc9b328b708cd908731b420c42f8
blackdizel <blackdizel@gmail.com>
2018-11-24 15:55:33 +0300

    note models

---
+++ app/src/main/java/org/byters/bctodo/model/ModelNote.kt
@@ -0,0 +1,6 @@
+package org.byters.bctodo.model
+
+import java.io.Serializable
+
+data class ModelNote(var title: String?, var body: String?) : Serializable {
+}
---
+++ app/src/main/java/org/byters/bctodo/model/ModelNoteCollection.kt
@@ -0,0 +1,6 @@
+package org.byters.bctodo.model
+
+import java.io.Serializable
+
+data class ModelNoteCollection(var notes:ArrayList<ModelNote>?) :Serializable{
+}
